1e0e831479d35aef8c0a3aac38a21ff73fdadeea
Laxman Rumba <rumbavai767@gmail.com>
2025-05-21 19:19:18 +0545

    Completed crud operation with code first

--- VideoGameApi/Controllers/VideoGameController.cs
+++ VideoGameApi/Controllers/VideoGameController.cs
@@ -38,6 +38,7 @@ namespace VideoGameApi.Controllers
             return Ok(videoGames);
         }
 
+        [HttpGet("{id}")]
         public ActionResult<VideoGame> GetVideoGameById(int id)
         {
             var game = videoGames.FirstOrDefault(g=>g.Id==id);
@@ -48,5 +49,47 @@ namespace VideoGameApi.Controllers
             return Ok(game);
         }
 
+        [HttpPost]
+        public ActionResult<VideoGame> AddVideoGame(VideoGame newGame)
+        {
+            if(newGame is null)
+            {
+                return BadRequest();
+            }
+            newGame.Id = videoGames.Max(g => g.Id) + 1;
+            videoGames.Add(newGame);
+            return CreatedAtAction(nameof(GetVideoGameById),new {id = newGame.Id},newGame);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateVideoGame(int id, VideoGame updateGame)
+        {
+            var game = videoGames.FirstOrDefault(g => g.Id == id);
+            if (game is null)
+            {
+                return BadRequest();
+            }
+
+            game.Title = updateGame.Title;
+            game.Platform = updateGame.Platform;
+            game.Publisher = updateGame.Publisher;
+            game.Developer = updateGame.Developer;
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+
+        public IActionResult DeleteVideoGame(int id)
+        {
+            var game = videoGames.FirstOrDefault(g => g.Id == id);
+            if(game is null)
+            {
+                return BadRequest();
+            }
+
+            videoGames.Remove(game);
+            return NoContent();
+
+        }
     }
 }
